8d5328163217350aa853af8f38fa7b1ee25014c2
Svetoslav Savov <svetoslav.savov@stanga.net>
2020-01-12 10:06:31 +0200

    Added rack-cors gem and setting it, editing json_responses

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -2,13 +2,16 @@ module Api
     module V1
         class ContactsController < ApplicationController
             def index
-                contacts = Contact.order('created_at DESC');
-                render json: {status: 'SUCCESS', message: 'Loaded contacts', data:contacts},status: :ok
+                ##contacts = Contact.order('created_at DESC');
+                ##render json: {status: 'SUCCESS', message: 'Loaded contacts', data:contacts},status: :ok
+                @contacts = Contact.all
+                json_response(data:@contacts)
             end
 
             def show
-               contact = Contact.find(params[:id])
-               render json: {status: 'SUCCESS', message: 'Loaded contact', data:contact},status: :ok
+               @contact = Contact.find(params[:id])
+               ##render json: {status: 'SUCCESS', message: 'Loaded contact', data:contact},status: :ok
+               json_response(data:@contact)
             end
         end
     end
